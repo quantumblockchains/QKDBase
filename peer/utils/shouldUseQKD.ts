eb4518933e99eada405cdb52c7d95098f6a9b0d7
quantumblockchains <dev@quantumblockchains.io>
2022-04-06 13:58:37 +0200

    Add dotenv to should use

--- peer/utils/shouldUseQKD.ts
+++ peer/utils/shouldUseQKD.ts
@@ -1,1 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
+
 export const shouldUseQKD = process.env.QKD_GET_KEY_URL;
--- peer/utils/shouldUseQRNG.ts
+++ peer/utils/shouldUseQRNG.ts
@@ -1,1 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
+
 export const shouldUseQRNG = process.env.QRNG_GET_RANDOM_ARRAY_URL && process.env.QRNG_GET_RANDOM_ARRAY_API_KEY;
